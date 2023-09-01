aa751b4d39df48f1d2724a3258e3dc94922d2bb4
maks <maxim_86nva@mail.ru>
2023-09-01 16:38:50 +0300

    init project

---
+++ src/main/java/hwcollectionsind13/hwcollectionsind13/HwCollectionsInd13Application.java
@@ -0,0 +1,13 @@
+package hwcollectionsind13.hwcollectionsind13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwCollectionsInd13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwCollectionsInd13Application.class, args);
+	}
+
+}
---
+++ src/test/java/hwcollectionsind13/hwcollectionsind13/HwCollectionsInd13ApplicationTests.java
@@ -0,0 +1,13 @@
+package hwcollectionsind13.hwcollectionsind13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwCollectionsInd13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
